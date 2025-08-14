134802521c1a5c48d20da473e653cb6ff0ada435
Kevin Vigor <kvigor@gmail.com>
2025-08-14 15:28:04 -0600

    Very basic timer interface works

--- embassy-riscv-hal/src/lib.rs
+++ embassy-riscv-hal/src/lib.rs
@@ -1,6 +1,12 @@
 #![no_std]
+
+use riscv::register::mstatus;
+
 mod time_driver;
 
 pub fn init() {
-    
+    time_driver::init();
+    unsafe {
+        mstatus::set_mie();
+    }
 }
--- examples/riscv/src/main.rs
+++ examples/riscv/src/main.rs
@@ -4,18 +4,16 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_time::Timer;
-//use embassy_riscv_hal::* as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    embassy_riscv_hal::init();
-    
     semihosting::println!("hello world");
-//    semihosting::sys::arm_compat::sys_exit(semihosting::sys::arm_compat::ExitReason::ADP_Stopped_ApplicationExit);
+    embassy_riscv_hal::init();
+    //    semihosting::sys::arm_compat::sys_exit(semihosting::sys::arm_compat::ExitReason::ADP_Stopped_ApplicationExit);
     loop {
-        info!("high");
+        semihosting::println!("high");
         Timer::after_millis(1000).await;
-        info!("low");
+        semihosting::println!("low");
         Timer::after_millis(1000).await;
     }
 }
